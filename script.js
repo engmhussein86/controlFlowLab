623045501db7a964161c66d7a37734c6f189f782
Mariam J. Hussein <engmhussein86@gmail.com>
2024-04-04 14:44:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log('running');
+
+let radius = 5;//meters
+const PI = 3.1415;
+const area = PI * radius * radius;
+console.log(`all area = ${area} square meters`);
+
+const eachPlantArea = 0.8;
+
+let starting = 20;
+
+console.log(`Starting plants Area = ${getPlantsArea(eachPlantArea,starting)} square meters`);
+
+// The plants double in number every week.
+
+let week1 = 1;//week1
+
+let plantsNumber = getPlantsNumber(week1,starting);
+let plantArea = getPlantsArea(eachPlantArea,plantsNumber);
+let percentage = (plantArea/area)*100;
+console.log(`plantsNumber after one week  =${plantsNumber} , area = ${plantArea} , Percentage = ${percentage}%`);
+console.log(`the action should be taken after one week ${predictGrowth(percentage)}`);
+
+
+/////////////// functions ////////////////////
+function getPlantsArea(eachPlantArea, plantsNumber) {
+    return eachPlantArea * plantsNumber;
+}
+///////////////////////////////////////////////
+function getPlantsNumber(weekNumber , plantsNumber) {
+    return plantsNumber * (2**weekNumber);
+}
+///////////////////////////////////////////////
+function predictGrowth(percentage){
+    let msg;
+    if(percentage >= 80){
+        msg = "Pruned";
+    }
+    else if(percentage >= 50 && percentage < 80){
+        msg = "Monitored";
+    }
+    else if(percentage < 50){
+        msg = "Planted";
+    }
+
+    return msg;
+}
+
+
